c078e126569e14de90310b03faf0b6d76389e176
CmtBoy <alaminislam.dev@gmail.com>
2024-06-15 23:58:27 +0600

    push

--- lib/main.dart
+++ lib/main.dart
@@ -9,42 +9,38 @@ import 'package:provider/provider.dart';
 import 'package:drivers_app/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main() async
-{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
-  
+  );
 
   //plus
   sharedPreferences = await SharedPreferences.getInstance();
 
   // await Firebase.initializeApp();
-     initilizeOnesignal();
+  initilizeOnesignal();
 
   runApp(
     MyApp(
-      child: ChangeNotifierProvider(
-        create: (context) => AppInfo(),
-        child: MaterialApp(
-          title: 'Driver App',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: const MySplashScreen(),
-          debugShowCheckedModeBanner: false,
+        child: ChangeNotifierProvider(
+      create: (context) => AppInfo(),
+      child: MaterialApp(
+        title: 'Driver App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      )
-    ),
+        home: const MySplashScreen(),
+        debugShowCheckedModeBanner: false,
+      ),
+    )),
   );
 }
-class MyApp extends StatefulWidget
-{
+
+class MyApp extends StatefulWidget {
   final Widget? child;
   MyApp({this.child});
-  static void restartApp(BuildContext context)
-  {
+  static void restartApp(BuildContext context) {
     context.findAncestorStateOfType<_MyAppState>()!.restartApp();
   }
 
@@ -52,14 +48,14 @@ class MyApp extends StatefulWidget
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp>
-{
+class _MyAppState extends State<MyApp> {
   Key key = UniqueKey();
-  void restartApp(){
+  void restartApp() {
     setState(() {
       key = UniqueKey();
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return KeyedSubtree(
@@ -69,13 +65,13 @@ class _MyAppState extends State<MyApp>
   }
 }
 
-initilizeOnesignal(){
-if(FirebaseAuth.instance.currentUser!=null){
+initilizeOnesignal() {
+  if (FirebaseAuth.instance.currentUser != null) {
     OneSignal.initialize("730ff581-8df1-415b-98da-0dab05dbd851");
-  OneSignal.Notifications.addPermissionObserver((state) {
-    print("Has permission $state");
-  });
-  OneSignal.login(FirebaseAuth.instance.currentUser!.uid);
-  OneSignal.Notifications.requestPermission(true);
-}
+    OneSignal.Notifications.addPermissionObserver((state) {
+      print("Has permission $state");
+    });
+    OneSignal.login(FirebaseAuth.instance.currentUser!.uid);
+    OneSignal.Notifications.requestPermission(true);
+  }
 }
--- lib/splashScreen/splash_screen.dart
+++ lib/splashScreen/splash_screen.dart
@@ -6,81 +6,69 @@ import 'package:drivers_app/global/global.dart';
 import 'package:drivers_app/mainScreens/main_screen.dart';
 import 'package:flutter/material.dart';
 
-
-class MySplashScreen extends StatefulWidget 
-{
+class MySplashScreen extends StatefulWidget {
   const MySplashScreen({Key? key}) : super(key: key);
 
   @override
   State<MySplashScreen> createState() => _MySplashScreenState();
 }
 
-
-
-class _MySplashScreenState extends State<MySplashScreen> 
-{
-  
-  startTimer()//how much time to display
+class _MySplashScreenState extends State<MySplashScreen> {
+  startTimer() //how much time to display
   {
-    Timer(const Duration(seconds: 3), () async
-    {
-      if(await fAuth.currentUser != null) //identify user register or not
-        {
-          currentFirebaseUser = fAuth.currentUser;
-          Navigator.push(context, MaterialPageRoute(builder: (c)=> MainScreen()));
-        }
-      else
-        {
-          Navigator.push(context, MaterialPageRoute(builder: (c)=> LoginScreen()));
-        }
-
+    Timer(const Duration(seconds: 3), () async {
+      if (await fAuth.currentUser != null) //identify user register or not
+      {
+        currentFirebaseUser = fAuth.currentUser;
+        Navigator.push(
+            context, MaterialPageRoute(builder: (c) => MainScreen()));
+      } else {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (c) => LoginScreen()));
+      }
     });
   }
-  
-  
+
   @override
-  void initState() { //called when go to any page
+  void initState() {
+    //called when go to any page
     super.initState(); //execute first/automatically
-    
+
     startTimer();
   }
 
+//stack
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return Material(
       child: Container(
-        color: Colors.black,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-
-              Image.asset("images/logo2.png"),
-
-              const SizedBox(height: 10,),
-
-              const Text(
-                "Welcome to",
-                //"Ride Sharing Application",
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
+          color: Colors.black,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("images/logo2.png"),
+                const SizedBox(
+                  height: 10,
                 ),
-              ),
-              const Text(
-                "UTM Ridex",
-                style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold
+                const Text(
+                  "Welcome to",
+                  //"Ride Sharing Application",
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
                 ),
-              )
-            ],
-          ),
-        )
-      ),
+                const Text(
+                  "UTM Ridex",
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          )),
     );
   }
 }
